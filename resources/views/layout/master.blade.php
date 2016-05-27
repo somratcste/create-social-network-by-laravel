d3f746ddf2fb3f266567a64aec4d652af9baf7a4
unknown <Somrat@Somrat.(none)>
2016-05-27 19:32:51 +0600

    Editing the post with jquery and bootstrap modal

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -2,8 +2,10 @@
 <html>
     <head>
         <title>@yield('title')</title>
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">\
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="{{URL::to('src/css/main.css')}}">
+
+        
 
     </head>
     <body>
@@ -12,8 +14,9 @@
             @yield('content')
         </div>
 
+        <script type='text/javascript' src='js/jquery-1.12.4.js'></script>
         <script type="text/javascript" src="js/bootstrap.min.js"></script>
-        
+        <script src="{{URL::to('src/js/app.js')}}" type="text/javascript" ></script>  
     </body>
 
 </html>
